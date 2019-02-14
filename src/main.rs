a13cc5e0451aa0aa1de11943299357deecad65a2
yo1000 <yoichi.kikuchi.aug@gmail.com>
2019-02-14 17:07:48 +0900

    Update service_fn interface

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,135 @@
+#![deny(warnings)]
+extern crate futures;
 extern crate hyper;
+extern crate pretty_env_logger;
 #[macro_use]
 extern crate serde_json;
+extern crate url;
 
 use std::collections::HashMap;
 
-use hyper::{Body, Method, Response, Server, StatusCode};
-// Used for map_err
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use futures::{future, Future, Stream};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::service::service_fn;
+use url::form_urlencoded;
+
+static INDEX: &[u8] = br#"
+<html>
+<body>
+<form action="post" method="post">
+    Name: <input type="text" name="name"><br>
+    Number: <input type="text" name="number"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
+"#;
+static MISSING: &[u8] = b"Missing field";
+static NOTNUMERIC: &[u8] = b"Number field is not numeric";
+
+// Using service_fn, we can turn this function into a `Service`.
+fn param_example(req: Request<Body>) -> Box<Future<Item=Response<Body>, Error=hyper::Error> + Send> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") | (&Method::GET, "/post") => {
+            Box::new(future::ok(Response::new(INDEX.into())))
+        },
+        (&Method::POST, "/post") => {
+            Box::new(req.into_body().concat2().map(|b| {
+                // Parse the request body. form_urlencoded::parse
+                // always succeeds, but in general parsing may
+                // fail (for example, an invalid post of json), so
+                // returning early with BadRequest may be
+                // necessary.
+                //
+                // Warning: this is a simplified use case. In
+                // principle names can appear multiple times in a
+                // form, and the values should be rolled up into a
+                // HashMap<String, Vec<String>>. However in this
+                // example the simpler approach is sufficient.
+                let params = form_urlencoded::parse(b.as_ref()).into_owned().collect::<HashMap<String, String>>();
+
+                // Validate the request parameters, returning
+                // early if an invalid input is detected.
+                let name = if let Some(n) = params.get("name") {
+                    n
+                } else {
+                    return Response::builder()
+                        .status(StatusCode::UNPROCESSABLE_ENTITY)
+                        .body(MISSING.into())
+                        .unwrap();
+                };
+                let number = if let Some(n) = params.get("number") {
+                    if let Ok(v) = n.parse::<f64>() {
+                        v
+                    } else {
+                        return Response::builder()
+                            .status(StatusCode::UNPROCESSABLE_ENTITY)
+                            .body(NOTNUMERIC.into())
+                            .unwrap();
+                    }
+                } else {
+                    return Response::builder()
+                        .status(StatusCode::UNPROCESSABLE_ENTITY)
+                        .body(MISSING.into())
+                        .unwrap();
+                };
+
+                // Render the response. This will often involve
+                // calls to a database or web service, which will
+                // require creating a new stream for the response
+                // body. Since those may fail, other error
+                // responses such as InternalServiceError may be
+                // needed here, too.
+                let body = format!("Hello {}, your number is {}", name, number);
+                Response::new(body.into())
+            }))
+        },
+        (&Method::GET, "/hello") => {
+            Box::new(future::ok(Response::builder()
+                .status(StatusCode::OK)
+                .header("X-HELLO", "world")
+                .body(Body::from(json!({
+                    "message": "Hello, World!"}).to_string()))
+                .unwrap()))
+        },
+        (&Method::GET, "/query_as_json") => {
+            let query_as_map = match req.uri().query() {
+                Some(it) => {
+                    it.split('&')
+                        .map(|q| q.split('=')
+                            .collect::<Vec<_>>())
+                        .filter(|q| q.len() >= 1)
+                        .map(|q| match q.len() {
+                            1 => { (q[0], "") }
+                            _ => { (q[0], q[1]) }
+                        })
+                        .collect::<HashMap<_, _>>()
+                }
+                None => { HashMap::new() }
+            };
+
+            Box::new(future::ok(Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .body(Body::from(json!(query_as_map).to_string()))
+                .unwrap()))
+        },
+        _ => {
+            Box::new(future::ok(Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap()))
+        }
+    }
+}
 
 fn main() {
-    let addr = ([127, 0, 0, 1], 3000).into();
+    pretty_env_logger::init();
 
-    let new_svc = || {
-        service_fn_ok(|_req|{
-            match(_req.method(), _req.uri().path()) {
-                (&Method::GET, "/hello") => {
-                    Response::builder()
-                        .status(StatusCode::OK)
-                        .header("X-HELLO", "world")
-                        .body(Body::from(json!({
-                            "message": "Hello, World!"}).to_string()))
-                        .unwrap()
-                },
-                (&Method::GET, "/query_as_json") => {
-                    let query_as_map = match _req.uri().query() {
-                        Some(it) => {
-                            it.split('&')
-                                .map(|q| q.split('=')
-                                    .collect::<Vec<_>>())
-                                .filter(|q| q.len() >= 1)
-                                .map(|q| match q.len() {
-                                    1 => { (q[0], "") }
-                                    _ => { (q[0], q[1]) }
-                                })
-                                .collect::<HashMap<_, _>>()
-                        }
-                        None => { HashMap::new() }
-                    };
-
-                    Response::builder()
-                        .status(StatusCode::OK)
-                        .header("Content-Type", "application/json; charset=utf-8")
-                        .body(Body::from(json!(query_as_map).to_string()))
-                        .unwrap()
-                },
-                (_, _) => {
-                    Response::builder()
-                        .status(StatusCode::NOT_FOUND)
-                        .body(Body::from("Not found"))
-                        .unwrap()
-                }
-            }
-        })
-    };
+    let addr = ([127, 0, 0, 1], 1337).into();
 
     let server = Server::bind(&addr)
-        .serve(new_svc)
+        .serve(|| service_fn(param_example))
         .map_err(|e| eprintln!("server error: {}", e));
 
     hyper::rt::run(server);
